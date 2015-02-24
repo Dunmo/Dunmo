f9b5d27ecb051c517038ebfb36d0d886666d48b9
Jeremy Meyer <jjman505@gmail.com>
2015-02-24 04:07:30 -0500

    added uuid, icloud

--- client/views/login/login.js
+++ client/views/login/login.js
@@ -1,6 +1,6 @@
 
 Template.login.events({
-  'click .btn': function (e) {
+  'click .btn-goog': function (e) {
     Meteor.loginWithGoogle({
       requestPermissions: ["email", "profile", "https://www.googleapis.com/auth/calendar", "https://www.googleapis.com/auth/tasks"],
       requestOfflineToken: true,
@@ -9,5 +9,9 @@ Template.login.events({
       if (err)
         Session.set('errorMessage', err.reason || 'Unknown error');
     });
+  },
+
+  'click .btn-appl': function (e) {
+    new iCloud('jjman505@gmail.com', 'hesus to the rescue G 23');
   }
 });
