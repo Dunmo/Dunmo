137c1923b71a452343725387bb6b47a31493450d
Michael Goldman <goldmandrummer@gmail.com>
2015-03-30 05:03:38 -0400

    input form doesn't validate, works using defaults

--- client/views/_partials/add-task-input/addTaskInput.js
+++ client/views/_partials/add-task-input/addTaskInput.js
@@ -6,9 +6,34 @@ Template.addTaskInput.rendered = function () {
 Template.addTaskInput.events({
   'submit form.add-task, click button.add-task': function (e) {
     $('#input-validation-msg').html('');
-    var $input = $(e.target).parents('.input-group').find('.form-control');
-    if( $input.length === 0 ) $input = $(e.target).find('input.add-task');
-    var val = $input.val();
+
+    var taskTitle = $('#taskTitle').val();
+    var taskHours = $('#taskHours').val();
+    var taskMinutes = $('#taskMinutes').val();
+    var taskDueAt = $('#taskDueAt').val();
+    var taskImportance = $('#taskImportance').val();
+
+    console.log("taskTitle: ", taskTitle);
+    console.log("taskHours: ", taskHours);
+    console.log("taskMinutes: ", taskMinutes);
+    console.log("taskDueAt: ", taskDueAt);
+    console.log("taskImportance: ", taskImportance);
+
+    if (taskTitle === '') taskTitle = 'Task title';
+    if (taskHours === '') taskHours = '0';
+    if (taskMinutes === '') taskMinutes = '30';
+    if (taskDueAt === '') taskDueAt = 'tomorrow';
+    if (taskImportance === '') taskImportance = 'not very';
+
+    var val = '';
+    val += taskTitle;
+    val += ' for ' + taskHours;
+    val += ' hours and ' + taskMinutes;
+    val += ' minutes due ' + taskDueAt;
+    val += ' ' + taskImportance + ' important';
+
+    console.log("val: ", val);
+
     var ret = Tasks.create(val, { ownerId: Meteor.userId() });
 
     console.log('ret: ', ret);
@@ -20,7 +45,13 @@ Template.addTaskInput.events({
       });
       return;
     };
-    $input.val('');
+
+    taskTitle = '';
+    taskHours = '';
+    taskMinutes = '';
+    taskDueAt = '';
+    taskImportance = '';
+
     gapi.syncTasksWithCalendar();
   }
 });
