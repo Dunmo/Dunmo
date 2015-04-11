7ffb1699e3692f58c3cf2f14cf37a15233229e3a
Jeremy Meyer <jjman505@gmail.com>
2015-04-11 09:04:25 -0400

    finished basic flows

--- client/views/layout/nav.js
+++ client/views/layout/nav.js
@@ -3,5 +3,15 @@ Template.nav.events({
   'click .logout': function (e) {
     var _ = Meteor.logout();
     FlowRouter.go('/index.html');
+  },
+
+  'click .taskView': function (e) {
+    csFlow.dismissFlow();
+    location.href = '/taskView';
+  },
+
+  'click .calendarSettings': function (e) {
+    tvFlow.dismissFlow();
+    location.href = '/calendarSettings';
   }
 });
