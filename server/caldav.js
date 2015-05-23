a26a4dfcd51e919d898e1d01695e6ba30ff4788f
Michael Goldman <goldmandrummer@gmail.com>
2015-05-23 11:10:46 -0500

    file add

---
+++ server/caldav.js
@@ -0,0 +1,25 @@
+
+Meteor.methods({
+  'getCalendar': function getCalendar(user) {
+    var CalDAVApi = Meteor.npmRequire('dav');
+    var caldav = new CalDAVApi({
+        version: "1.7.6"
+    });
+
+    var Cals = Async.runSync(function(done) {
+      caldav.calendarList.list(null, function(err, data) {
+        done(null, data);
+      });
+    });
+
+    var DunmoCal = Async.runSync(function(done) {
+      var id = Cals.find({ summary: "Dunmo Tasks" }).id;
+      caldav.calendars.get(id, function(err, data) {
+        done(null, data);
+      });
+    });
+
+    console.log("DunmoCal.result: ", DunmoCal.result);
+    return DunmoCal.result;
+  }
+});
