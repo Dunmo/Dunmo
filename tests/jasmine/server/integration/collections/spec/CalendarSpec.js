955ff811f623392e50b8f496bc9a193b5f01213d
Jeremy Meyer <jjman505@gmail.com>
2015-05-30 11:25:33 -0400

    added CalendarSpec skeleton

---
+++ tests/jasmine/server/integration/collections/spec/CalendarSpec.js
@@ -0,0 +1,40 @@
+
+describe('calendar', function () {
+  // No Instance Methods
+});
+
+describe('Calendars', function () {
+
+  describe('create', function () {
+
+    it('should', function () {
+
+    });
+
+  });
+
+  describe('insert', function () {
+
+    it('should', function () {
+
+    });
+
+  });
+
+  describe('update', function () {
+
+    it('should', function () {
+
+    });
+
+  });
+
+  describe('updateOrCreate', function () {
+
+    it('should', function () {
+
+    });
+
+  });
+
+});
