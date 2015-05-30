ab9f9ed89ff8024d2e8475b979fcbad92201540f
Jeremy Meyer <jjman505@gmail.com>
2015-05-30 15:12:37 -0400

    added ApiSpec skeleton

---
+++ tests/jasmine/client/integration/api/spec/ApiSpec.js
@@ -0,0 +1,28 @@
+
+describe('/api', function () {
+
+  describe('GET /subscribers/:userId', function () {
+
+    it('should', function () {
+
+    });
+
+  });
+
+  describe('GET /subscribers', function () {
+
+    it('should', function () {
+
+    });
+
+  });
+
+  describe('GET /subscribers/:email', function () {
+
+    it('should', function () {
+
+    });
+
+  });
+
+});
