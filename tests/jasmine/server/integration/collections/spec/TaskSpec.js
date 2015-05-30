15ec9a9b27c428868f52741ce0de9955703d4156
Jeremy Meyer <jjman505@gmail.com>
2015-05-30 12:32:53 -0400

    moved setRemoved test to CollectionsDefaultSpec

--- tests/jasmine/server/integration/collections/spec/TaskSpec.js
+++ tests/jasmine/server/integration/collections/spec/TaskSpec.js
@@ -8,39 +8,6 @@ describe('task', function () {
     _task      = Tasks.findOne(taskId);
   });
 
-
-  describe('setRemoved', function () {
-
-    it('should default to setting .isRemoved = true', function () {
-      _task.setRemoved();
-
-      var expectedDefault = true;
-      expect(_task.isRemoved).toEqual(expectedDefault);
-
-      var updatedTask = Tasks.findOne(_task._id);
-      expect(updatedTask.isRemoved).toEqual(expectedDefault);
-    });
-
-    it('should set .isRemoved to the given value', function () {
-      var updatedTask, expectedDefault;
-
-      expectedDefault = true;
-      _task.setRemoved(expectedDefault);
-      expect(_task.isRemoved).toEqual(expectedDefault);
-
-      updatedTask = Tasks.findOne(_task._id);
-      expect(updatedTask.isRemoved).toEqual(expectedDefault);
-
-      expectedDefault = false;
-      _task.setRemoved(expectedDefault);
-      expect(_task.isRemoved).toEqual(expectedDefault);
-
-      updatedTask = Tasks.findOne(_task._id);
-      expect(updatedTask.isRemoved).toEqual(expectedDefault);
-    });
-
-  });
-
   describe('setDone', function () {
 
     it('should default to setting .isDone = true', function () {
