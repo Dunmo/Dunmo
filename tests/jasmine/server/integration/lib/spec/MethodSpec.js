8e6064dfd1ba6500c3b224bce1e5f00dd8419bbc
Jeremy Meyer <jjman505@gmail.com>
2015-05-30 14:42:44 -0400

    added MethodSpec skeleton

---
+++ tests/jasmine/server/integration/lib/spec/MethodSpec.js
@@ -0,0 +1,36 @@
+
+describe('methods', function () {
+
+  describe('postSync', function () {
+
+    it('should', function () {
+
+    });
+
+  });
+
+  describe('getSync', function () {
+
+    it('should', function () {
+
+    });
+
+  });
+
+  describe('createReferral', function () {
+
+    it('should', function () {
+
+    });
+
+  });
+
+  describe('fetchMailingList', function () {
+
+    it('should', function () {
+
+    });
+
+  });
+
+});
