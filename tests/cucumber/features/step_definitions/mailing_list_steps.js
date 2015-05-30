1aed24498aaa98e51e5a771d87f48e78516696b8
Jeremy Meyer <jjman505@gmail.com>
2015-05-29 22:06:24 -0400

    added mailing list feature spec

---
+++ tests/cucumber/features/step_definitions/mailing_list_steps.js
@@ -0,0 +1,51 @@
+(function () {
+
+  'use strict';
+
+  module.exports = function () {
+
+    // You can use normal require here, cucumber is NOT run in a Meteor context (by design)
+    var url    = require('url');
+    var _      = require('lodash');
+    var assert = require('chai').assert;
+
+    this.When(/^I type "([^"]*)" into the email input$/, function (email, callback) {
+      var emailInputSelector = '.mailing-list .email';
+      this.client.waitForVisible(emailInputSelector)
+                 .setValue(emailInputSelector, email)
+                 .getValue(emailInputSelector).should.become(email)
+                 .and.notify(callback);
+    });
+
+    this.Then(/^the email "([^"]*)" should be added to the database$/, function (email, callback) {
+      // callback.pending();
+      this.client.executeAsync(function(done) {
+        $.get('/api/emails')
+         .error(function(err, msg, xhr) {
+            if(err) { console.error(err); return; }
+          })
+         .success(function(res, msg, xhr) {
+            done(res);
+          });
+      }, function(err, res) {
+        if(err) { console.error(err); return; }
+        var mailingList     = res.value;
+        mailingList         = JSON.parse(mailingList);
+        var emails          = _.pluck(mailingList, 'email');
+        var isInMailingList = _.include(emails, email);
+        assert.isTrue(isInMailingList);
+        callback();
+      });
+    });
+
+    this.Then(/^I should see a confirmation message$/, function (callback) {
+      // Write code here that turns the phrase above into concrete actions
+      // callback.pending();
+      var emailInputSelector = '.mailing-list .email';
+      this.client.waitForVisible(emailInputSelector)
+          .getAttribute(emailInputSelector, 'placeholder').should.become('Success!');
+    });
+
+  };
+
+})();
