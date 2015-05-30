27d88401cbe35139c0558db881787d4932c35eb4
Jeremy Meyer <jjman505@gmail.com>
2015-05-30 12:33:16 -0400

    created CollectionsDefaultSpec skeleton

---
+++ tests/jasmine/server/integration/collections/spec/CollectionsDefaultSpec.js
@@ -0,0 +1,62 @@
+
+describe('collectionsDefault', function () {
+
+  // TODO: Add stub
+
+  describe('setRemoved', function () {
+
+    // it('should default to setting .isRemoved = true', function () {
+    //   _task.setRemoved();
+
+    //   var expectedDefault = true;
+    //   expect(_task.isRemoved).toEqual(expectedDefault);
+
+    //   var updatedTask = Tasks.findOne(_task._id);
+    //   expect(updatedTask.isRemoved).toEqual(expectedDefault);
+    // });
+
+    // it('should set .isRemoved to the given value', function () {
+    //   var updatedTask, expectedDefault;
+
+    //   expectedDefault = true;
+    //   _task.setRemoved(expectedDefault);
+    //   expect(_task.isRemoved).toEqual(expectedDefault);
+
+    //   updatedTask = Tasks.findOne(_task._id);
+    //   expect(updatedTask.isRemoved).toEqual(expectedDefault);
+
+    //   expectedDefault = false;
+    //   _task.setRemoved(expectedDefault);
+    //   expect(_task.isRemoved).toEqual(expectedDefault);
+
+    //   updatedTask = Tasks.findOne(_task._id);
+    //   expect(updatedTask.isRemoved).toEqual(expectedDefault);
+    // });
+
+  });
+
+  describe('update', function () {
+
+    it('', function () {
+
+    });
+
+  });
+
+  describe('fetch', function () {
+
+    it('', function () {
+
+    });
+
+  });
+
+  describe('findBy', function () {
+
+    it('', function () {
+
+    });
+
+  });
+
+});
