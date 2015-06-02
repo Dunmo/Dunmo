c524e8800ea11dec20ab5ebbb1c5ab4a1c8feedf
Jeremy Meyer <jjman505@gmail.com>
2015-06-02 11:51:59 -0400

    added recent task events method

--- collections/Events.js
+++ collections/Events.js
@@ -15,7 +15,11 @@ Events.helpers({
 
   setRemoved: collectionsDefault.setRemoved(),
 
-  update: collectionsDefault.update(Events)
+  update: collectionsDefault.update(Events),
+
+  task: function () {
+    return Tasks.findOne(this.taskId);;
+  }
 
 });
 
@@ -27,6 +31,13 @@ Events.createOrUpdate = function (obj) {
     });
   } else if(typeof(obj) === 'object') {
     obj.googleEventId = obj.googleEventId || obj.id;
+    obj.isRemoved     = obj.isRemoved     || obj.status === 'cancelled'
+    obj.start         = new Date(obj.start.dateTime);
+    obj.end           = new Date(obj.end.dateTime);
+    obj.created       = new Date(obj.created);
+    obj.updated       = new Date(obj.updated);
+    obj.isTransparent = obj.isTransparent || obj.transparency === 'transparent';
+    obj.title         = obj.title         || obj.summary;
     var event = Events.findOne({ googleEventId: obj.googleEventId });
     if(event) {
       return Events.update(event._id, { $set: obj });
@@ -43,19 +54,20 @@ Events.fetch = collectionsDefault.fetch(Events);
 Events.fetchActive = collectionsDefault.fetchActive(Events);
 
 // Client Only; Requires google api
-Events.matchGoogle = function (selector, options) {
+Events.syncWithGoogle = function (selector, options) {
   var events = Events.fetch(selector, options);
   events.forEach(function (event) {
     gapi.getEvent(event, function (googleEvent) {
-      console.log('event: ', event);
-      // Events.createOrUpdate(googleEvent);
+      Events.createOrUpdate(googleEvent);
     });
   });
 };
 
 // Client Only; Requires google api
-Events.matchGoogleActive = function (selector, options) {
-  Events.matchGoogle({ isRemoved: { $ne: true } });
+Events.syncActiveWithGoogle = function (selector, options) {
+  selector = selector || {};
+  selector.isRemoved = { $ne: true };
+  Events.syncWithGoogle(selector, options);
 };
 
 Events.taskEvents = {};
@@ -64,4 +76,21 @@ Events.taskEvents.find = function (selector, options) {
   selector = selector || {};
   selector.taskId = { $exists: true };
   return Events.find(selector);
+};
+
+Events.taskEvents.recent = function (selector, options) {
+  selector = selector || {};
+  _.extend(selector, {
+    taskId: { $exists: true },
+    needsReviewed: true,
+    end: { $lt: new Date() }
+  });
+  return Events.fetchActive(selector, options);
+};
+
+Events.getTasks = function (events) {
+  var taskIds = events.map(function (e) { return e.taskId; });
+  var taskIds = _.uniq(taskIds);
+  var tasks   = Tasks.findAllById(taskIds);
+  return tasks;
 };
