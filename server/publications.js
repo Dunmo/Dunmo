cfbd2110830a089381b7b452f7ecae540d316bab
Jeremy Meyer <jjman505@gmail.com>
2015-06-01 22:46:54 -0400

    fixed typo

--- server/publications.js
+++ server/publications.js
@@ -6,5 +6,6 @@ Meteor.publish(allClients, function () {
   var currentUser = Meteor.users.find({ _id: this.userId });
   var calendars   = Calendars.find({ ownerId: this.userId });
   var tasks       = Tasks.find({ ownerId: this.userId });
-  return [currentUser, calendars, tasks];
+  var events      = Events.find({ ownerId: this.userId });
+  return [currentUser, calendars, tasks, events];
 });
