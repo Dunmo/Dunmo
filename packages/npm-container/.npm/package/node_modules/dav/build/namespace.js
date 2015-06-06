1bc30788b4f921346ee80b779e174bc6bf282166
Michael Goldman <goldmandrummer@gmail.com>
2015-06-05 21:39:16 -0400

    packages and such

---
+++ packages/npm-container/.npm/package/node_modules/dav/build/namespace.js
@@ -0,0 +1,13 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', {
+  value: true
+});
+var CALENDAR_SERVER = 'http://calendarserver.org/ns/';
+exports.CALENDAR_SERVER = CALENDAR_SERVER;
+var CALDAV = 'urn:ietf:params:xml:ns:caldav';
+exports.CALDAV = CALDAV;
+var CARDDAV = 'urn:ietf:params:xml:ns:carddav';
+exports.CARDDAV = CARDDAV;
+var DAV = 'DAV:';
+exports.DAV = DAV;
