1843fa93a4c9fb2ec46f09456974f3589eb4c5a0
Jeremy Meyer <jjman505@gmail.com>
2015-06-07 12:12:32 -0400

    added Daylist class

--- collections/lib/_declarations.js
+++ collections/lib/_declarations.js
@@ -1,5 +1,6 @@
 Calendars    = new Mongo.Collection('calendars');
 Events       = new Mongo.Collection('events');
+Daylists     = {};
 Freetimes    = new Mongo.Collection('freetimes');
 Subscribers  = new Mongo.Collection('subscribers');
 Tasks        = new Mongo.Collection('tasks');
