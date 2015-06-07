5fd2eba6b5463e5e925c24a3c928979f08e7a342
Jeremy Meyer <jjman505@gmail.com>
2015-06-07 07:09:00 -0400

    fixed typo

--- client/views/taskView/helpers.js
+++ client/views/taskView/helpers.js
@@ -5,10 +5,10 @@ resetTaskListItemWidths = function () {
   var cl = '.remaining';
   var textWidth = _.max($('.task .line-prop'+cl+' .task-text').toArray().map(function (obj) { return $(obj).width() }));
   var iconWidth = $('.task .line-prop'+cl+' .fa-wrapper').width();
-  $('.task .line-prop'+cl+'').width(textWidth + iconWidth + 10);
+  $('.task .line-prop'+cl).width(textWidth + iconWidth + 10);
 
   cl = '.due';
   textWidth = _.max($('.task .line-prop'+cl+' .task-text').toArray().map(function (obj) { return $(obj).width() }));
   iconWidth = $('.task .line-prop'+cl+' .fa-wrapper').width();
-  $('.task .line-prop'+cl+'').width(textWidth + iconWidth + 10);
+  $('.task .line-prop'+cl).width(textWidth + iconWidth + 10);
 };
