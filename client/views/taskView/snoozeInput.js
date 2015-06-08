4fb295d672058f593be7ded16e3236523b2cbc67
Jeremy Meyer <jjman505@gmail.com>
2015-06-07 21:41:40 -0400

    added snoozeInput template

---
+++ client/views/taskView/snoozeInput.js
@@ -0,0 +1,10 @@
+
+Template.snoozeInput.rendered = function() {
+  $('#datetimepicker').datetimepicker({
+    // sideBySide: true,
+    widgetPositioning: {
+      horizontal: 'right'
+    },
+    widgetParent: '.datetime-container'
+  });
+};
