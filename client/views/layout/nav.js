40e2ec0d68b213f7799913a13b6e6a0e24db8104
Jeremy Meyer <jjman505@gmail.com>
2015-06-07 22:37:57 -0400

    merge

--- client/views/layout/nav.js
+++ client/views/layout/nav.js
@@ -3,5 +3,13 @@ Template.nav.events({
   'click .logout': function (e) {
     var _ = Meteor.logout();
     FlowRouter.go('/index.html');
+  },
+
+  'click .taskView': function (e) {
+    location.href = '/taskView';
+  },
+
+  'click .calendarSettings': function (e) {
+    location.href = '/calendarSettings';
   }
 });
