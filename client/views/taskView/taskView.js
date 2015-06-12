58da5672c5369f90245fe19edb35e144279d3a81
Jeremy Meyer <jjman505@gmail.com>
2015-06-11 20:35:39 -0400

    added onboarding tasks

--- client/views/taskView/taskView.js
+++ client/views/taskView/taskView.js
@@ -26,16 +26,27 @@ Template.taskView.onRendered(function () {
     heap.identify({ name: user.profile.name,
                     email: user.primaryEmailAddress() });
 
+    if(!user.hasOnboarded('taskView')) {
+      Tasks.create([
+        'Mark this task done by clicking the green check button for 5 minutes due at midnight somewhat important',
+        'Snooze this task by clicking the yellow button for 5 minutes due at midnight somewhat important',
+        'Delete this task by clicking the red button for 5 minutes due at midnight somewhat important',
+        'Edit this task by clicking the grey button for 5 minutes due at midnight somewhat important',
+        'Add more tasks using the inputs above for 5 minutes due at midnight somewhat important',
+        'Check your Google calendar to see your tasks for 5 minutes due at midnight somewhat important',
+        'Click the sync tasks action in the upper right if your calendar events change for 5 minutes due at midnight somewhat important',
+        'Check out the settings page for more options for 5 minutes due at midnight somewhat important'
+      ], { ownerId: user._id });
+      user.setHasOnboarded('taskView', true);
+    }
+
     if(user.lastReviewed() < Date.startOfToday()) {
       console.log('setting tasks to review...');
       Tasks.setNeedsReviewed();
       user.setLastReviewed(Date.now());
     }
-
-    // $(document).on("onboarded:flow:afterComplete", function (e, flow, step) {
-    //   $(".overlay").addClass("hidden")
-    // });
   }
+
   Session.set('lastRendered', Date.floorMinute(Date.now()));
   Session.set('snoozeActive', '');
 
@@ -67,6 +78,10 @@ Template.taskView.helpers({
 
   anyUpcomingTasks: function () {
     return Meteor.user().upcomingTodos().count() > 0;
+  },
+
+  syncTitle: function () {
+    return Session.get('isSyncing') ? 'Syncing Tasks...' : 'Sync Tasks';
   },
 
   faSpinClass: function () {
