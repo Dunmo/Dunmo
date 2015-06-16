93e33f778dee4509c532e23afb8b1286d1d98d9d
Jeremy Meyer <jjman505@gmail.com>
2015-06-16 11:41:19 -0400

    removed old function

--- client/views/taskView/helpers.js
+++
@@ -1,6 +0,0 @@
-
-resetTaskListItemWidths = function () {
-  window.setTimeout(function () {
-    $('.task .title').width(Math.ceil(_.max($('.task .title .task-text').toArray().map(function (obj) { return $(obj).width() })))+24);
-  }, 100);
-};
