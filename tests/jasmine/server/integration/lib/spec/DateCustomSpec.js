8c59cb04a1b0bd6bbd18defe780e6a2ec25ac3b8
Jeremy Meyer <jjman505@gmail.com>
2015-06-19 10:49:20 -0400

    added tests for Date.formatGoog

--- tests/jasmine/server/integration/lib/spec/DateCustomSpec.js
+++ tests/jasmine/server/integration/lib/spec/DateCustomSpec.js
@@ -1,39 +1,43 @@
 
-describe('date', function () {
-
-  describe('startOfDay', function () {
-
-    it('should', function () {
-      pending();
-    });
-
-  });
-
-  describe('endOfDay', function () {
-
-    it('should', function () {
-      pending();
-    });
-
-  });
-
-  describe('isToday', function () {
-
-    it('should', function () {
-      pending();
-    });
-
-  });
-
-});
-
 describe('Date', function () {
+  var dateString, isoString;
+
+  beforeEach(function () {
+    dateString = 'Fri Jun 19 2015 10:23:27 GMT-0400 (EDT)';
+    isoString  = '2015-06-19T14:23:27.000Z';
+  });
 
   describe('formatGoog', function () {
 
-    it('should', function () {
-      pending();
-    });
+    describe('when given a number of milliseconds', function () {
+      var input, expected;
+
+      beforeEach(function () {
+        input    = Number(new Date(dateString));
+        expected = isoString;
+      });
+
+      it('should work', function () {
+        var ret = Date.formatGoog(input);
+        expect(ret).toEqual(expected);
+      });
+
+    })
+
+    describe('when given a date', function () {
+      var input, expected;
+
+      beforeEach(function () {
+        input    = new Date(dateString);
+        expected = isoString;
+      });
+
+      it('should work', function () {
+        var ret = Date.formatGoog(input);
+        expect(ret).toEqual(expected);
+      });
+
+    })
 
   });
 
