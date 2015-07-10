bed138beda8c3b69b002037305525a770164d255
Richard <r.shiazure@gmail.com>
2015-07-09 22:53:02 -0400

    Hid irrelevant sections, changed presentation, copy changes

--- public/js/landing.js
+++ public/js/landing.js
@@ -39,26 +39,26 @@ $(function () {
 
 // BOT
 
-$('.landing-meet__bot-container').waypoint(function () {
-    $('.landing-meet__bot').toggleClass('landing-meet__bot--sticky').css({ "bottom": "auto" });
-}, {
-    offset: '64px'
-}) // makes bot sticky
-$('.landing-team').waypoint(function () {
-    var difference = $('.landing-team').offset().top - ($('.landing-meet__bot').offset().top + $('.landing-meet__bot').height());
-    $('.landing-meet__bot').css({ "bottom": difference + "px" }).toggleClass('landing-meet__bot--sticky landing-meet__bot--end');
-}, {
-    offset: '100%'
-}) // stops bot above end of section
+//$('.landing-meet__bot-container').waypoint(function () {
+//    $('.landing-meet__bot').toggleClass('landing-meet__bot--sticky').css({ "bottom": "auto" });
+//}, {
+//    offset: '64px'
+//}) // makes bot sticky
+//$('.landing-team').waypoint(function () {
+//    var difference = $('.landing-team').offset().top - ($('.landing-meet__bot').offset().top + $('.landing-meet__bot').height());
+//    $('.landing-meet__bot').css({ "bottom": difference + "px" }).toggleClass('landing-meet__bot--sticky landing-meet__bot--end');
+//}, {
+//    offset: '100%'
+//}) // stops bot above end of section
 
 // BOTSPEAK
 
-$('.landing-meet').waypoint(function () {
-    $('.landing-meet__slide').toggle();
-}, {
-    offset: '75%'
-})
+//$('.landing-meet').waypoint(function () {
+//    $('.landing-meet__slide').toggle();
+//}, {
+//    offset: '75%'
+//})
 
 // INITIALIZE SKROLLR
 
-var s = skrollr.init({ forceHeight: false });
+//var s = skrollr.init({ forceHeight: false });
