2f78e986b75dce69fb78daf8d159be72650937a4
Jeremy Meyer <jjman505@gmail.com>
2015-07-17 23:13:35 -0400

    added login form and fixed styling and events
    increased font size for readability

--- client/views/login/login.js
+++ client/views/login/login.js
@@ -1,4 +1,18 @@
 
+function synchronize(src, dest) {
+  $('.nav-tabs > li').removeClass('active');
+  $('.notice').html('');
+  $(dest + '-link').addClass('active');
+  $('form' + src).hide();
+  $('form' + dest).show();
+  var $src = $('form' + src);
+  var email = $src.find('input.email').val();
+  var password = $src.find('input.password').val();
+  var $dest = $('form' + dest);
+  $dest.find('input.email').val(email);
+  $dest.find('input.password').val(password);
+};
+
 Template.login.helpers({
   'loggedIn': function() {
     return Meteor.userId();
@@ -6,6 +20,25 @@ Template.login.helpers({
 });
 
 Template.login.events({
+
+  'click .signup-link': function (e) {
+    synchronize('.login', '.signup');
+  },
+
+  'click .login-link': function (e) {
+    synchronize('.signup', '.login');
+  },
+
+  'click form.login button.login': function (e) {
+    console.log('e: ', e);
+
+  },
+
+  'click form.signup button.signup': function (e) {
+    console.log('e: ', e);
+
+  },
+
   'click .btn-gplus': function (e) {
     Meteor.loginWithGoogle({
       requestPermissions: ["email", "profile", "https://www.googleapis.com/auth/calendar", "https://www.googleapis.com/auth/tasks"],
@@ -16,4 +49,5 @@ Template.login.events({
       else     location.href = '/taskView';
     });
   }
+
 });
