0e8db32c7dc2de9381dabfe7224cc3bcc43420b7
Jeremy Meyer <jjman505@gmail.com>
2015-07-19 20:51:48 -0400

    wired up sidebar

--- client/views/sidebar/sidebar.js
+++ client/views/sidebar/sidebar.js
@@ -1,20 +1,27 @@
 
+function isActive (val) { return Session.get('task-filter')    === val ||
+                                 Session.get('project-filter') === val }
+
+Template.sidebar.helpers({
+  user: function () { return Meteor.user(); },
+  activeClass: function (filter) {
+    if(Session.get('active-sidebar-section') === filter) return 'app-sidebar__section--active';
+    else return isActive(filter) ? 'app-sidebar__subsection__item--active' : '';
+  }
+});
+
 Template.sidebar.events({
 
   'click .app-sidebar__section__heading': function (e) {
-    var self = e.target;
-    $('.app-sidebar__section').removeClass('app-sidebar__section--active');
-    $('.app-sidebar__subsection__item--active').removeClass('app-sidebar__subsection__item--active');
-    $(self).parent().addClass('app-sidebar__section--active');
-    $(self).parent().children('ul').children(':first-child').addClass('app-sidebar__subsection__item--active');
+    var slug = $(e.target).parents('.app-sidebar__section').data('section');
+    FlowRouter.go('/' + slug);
   },
 
   'click .app-sidebar__subsection__item': function (e) {
     var self = e.target;
-    $('.app-sidebar__section').removeClass('app-sidebar__section--active');
-    $('.app-sidebar__subsection__item--active').removeClass('app-sidebar__subsection__item--active');
-    $(self).parent().parent().addClass('app-sidebar__section--active');
-    $(self).addClass('app-sidebar__subsection__item--active');
+    var section = $(self).parents('.app-sidebar__section').data('section');
+    var slug = $(self).data('href');
+    FlowRouter.go('/' + section + '/' + slug);
   }
 
 });
