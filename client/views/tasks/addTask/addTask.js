4e9d4e6935638593689b0519ee4270e365946f23
Jeremy Meyer <jjman505@gmail.com>
2015-09-19 04:10:48 +0900

    hooked up add task

--- client/views/tasks/addTask/addTask.js
+++ client/views/tasks/addTask/addTask.js
@@ -34,3 +34,33 @@ View.helpers({
     return new Date();
   }
 });
+
+View.events({
+  'submit form.app-addtask': function (e, t) {
+    e.preventDefault();
+    var $parent = $('.app-addtask');
+    var importance = rankVar.get();
+    var title = $parent.find('input.app-addtask__content--title').val();
+    var hours = Number($parent.find('input.app-addtask__content--duration-hour').val());
+    console.log('hours: ', hours);
+    var mins = Number($parent.find('input.app-addtask__content--duration-minute').val());
+    console.log('mins: ', mins);
+    var duration = moment.duration({ hours: hours, minutes: mins }).asMilliseconds();
+    var duedate = $parent.find('input.app-addtask__content--due').val();
+    console.log('duedate: ', duedate);
+    var dueAt = moment(duedate).toDate();
+    console.log('importance, title, duration, dueAt: ', importance, title, duration, dueAt);
+
+    Tasks.create({
+      ownerId: Meteor.userId(),
+      title: title,
+      importance: importance,
+      dueAt: dueAt,
+      remaining: duration
+    });
+
+    Session.set('add-task-is-active', false);
+
+    return false;
+  }
+});
