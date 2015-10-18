9d40b95fba7bc6ae2ec15eb52c6bd0d29b4ae1ef
Richard <r.shiazure@gmail.com>
2015-10-18 16:05:12 -0400

    Improved features section

--- client/views/landing/landing.js
+++ client/views/landing/landing.js
@@ -5,12 +5,66 @@ View.onRendered(function () {
     var $nav = $('.landing-navigation');
     var fnav = 'landing-navigation--fixed';
     
-    var waypoint = new Waypoint({
+    var navigation = new Waypoint({
         element: document.getElementById('hero'),
         handler: function(direction) {
             $nav.toggleClass(fnav);
         },
         offset: -10
+    })
+    
+    var slide = {
+        opacity: 1,
+        right: 0
+    };
+    
+    var slideR = {
+        opacity: 0,
+        right: '-50%'
+    };
+        
+    var duration = 500;
+    
+    var featureAnimate = function(id, direction) {
+        if (direction == 'down') {
+            $('#features' + id).animate(slide, duration);
+        } else if (direction == 'up') {
+            $('#features' + id).animate(slideR, duration);
+        }
+    }
+    
+    var featureOffset = 400;
+    
+    var features1 = new Waypoint({
+        element: document.getElementById('features1'),
+        handler: function(direction) {
+            featureAnimate(1, direction);
+        },
+        offset: featureOffset
+    })
+    
+    var features2 = new Waypoint({
+        element: document.getElementById('features2'),
+        handler: function(direction) {
+            featureAnimate(2, direction);
+        },
+        offset: featureOffset
+    })
+    
+    var features3 = new Waypoint({
+        element: document.getElementById('features3'),
+        handler: function(direction) {
+            featureAnimate(3, direction);
+        },
+        offset: featureOffset
+    })
+    
+    var features4 = new Waypoint({
+        element: document.getElementById('features4'),
+        handler: function(direction) {
+            featureAnimate(4, direction);
+        },
+        offset: featureOffset
     })
       
 });
