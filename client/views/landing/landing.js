c69967e5a0fad34fb4dadd01d2a78525a7f73c73
Jeremy Meyer <jjman505@gmail.com>
2015-10-23 02:02:11 +0900

    added subscribe event to landing page fixed whitespace in landing.html
    fixed whitespace in landing.js
    
    added subscribe event to landing page

--- client/views/landing/landing.js
+++ client/views/landing/landing.js
@@ -1,25 +1,67 @@
+
+var subscribeLoading = new ReactiveVar();
+var subscribeDone = new ReactiveVar();
+
 var View = Template.landing;
+
+View.onCreated(function () {
+  subscribeLoading.set(false);
+  subscribeDone.set(false);
+});
 
 View.onRendered(function () {
 
-    // SMOOTH SCROLLING
-    
-    Meteor.defer(function () {
-    
+  // SMOOTH SCROLLING
 
-//        $('a[href*=#]:not([href=#])').click(function () {
-//          if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-//            var target = $(this.hash);
-//            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-//            if (target.length) {
-//              $('html,body').animate({
-//                scrollTop: target.offset().top
-//              }, 750);
-//              return false;
-//            }
-//          }
-//        });
-        
-    });
-      
+  // Meteor.defer(function () {
+
+  //   $('a[href*=#]:not([href=#])').click(function () {
+  //     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+  //       var target = $(this.hash);
+  //       target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+  //       if (target.length) {
+  //         $('html,body').animate({
+  //           scrollTop: target.offset().top
+  //         }, 750);
+  //         return false;
+  //       }
+  //     }
+  //   });
+
+  // });
+
+});
+
+View.helpers({
+  subscribeLoading: function () {
+    return subscribeLoading.get();
+  },
+
+  subscribeDone: function () {
+    return subscribeDone.get();
+  },
+
+  subscribeBtnText: function () {
+    return subscribeDone.get() ? 'Signed Up' : 'Sign Me Up!';
+  }
+});
+
+View.events({
+  'click .landing-subscribe__form__submit, submit #subscription': function (e, t) {
+    e.preventDefault();
+    subscribeLoading.set(true);
+
+    var email = $('.landing-subscribe__form__input[name="email"]').val();
+
+    Meteor.setTimeout(function () {
+      Meteor.call('mailing-list/subscribe', email, function (err, res) {
+        if(err) {
+          console.error('[subscribe] Error:', err);
+        } else {
+          subscribeDone.set(true);
+        }
+        subscribeLoading.set(false);
+      });
+    }, 500);
+  }
 });
