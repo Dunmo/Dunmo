191466a2030297a6b751186c4fb1db176b4d5d7d
Jeremy Meyer <jjman505@gmail.com>
2015-12-06 17:32:54 +0900

    publish based on ownerId, not userId

--- server/publications.js
+++ server/publications.js
@@ -5,14 +5,9 @@ var allClients = null;
 Meteor.publish(allClients, function () {
   var userId = this.userId;
   var publications = [];
-  publications.push(Users.find({ _id:     this.userId }));
+  publications.push(Users.find({ _id: userId }));
   [Calendars, Events, Projects, Tasks, TaskComments].forEach(function (collection) {
-    publications.push(collection.findAll({
-      $or: [
-        { ownerId: userId },
-        { userId: userId  }
-      ]
-    }));
+    publications.push(collection.findAll({ ownerId: userId }));
   });
   return publications;
 });
