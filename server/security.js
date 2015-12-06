7ceca114f5da552b59a2b257622c20831d9cc2ab
Jeremy Meyer <jjman505@gmail.com>
2015-12-06 17:34:38 +0900

    added example permits

---
+++ server/security.js
@@ -0,0 +1,7 @@
+
+// Calenders.permit(['insert', 'update', 'remove']).apply();
+// Events.permit(['insert', 'update', 'remove']).apply();
+// Projects.permit(['insert', 'update', 'remove']).apply();
+// TaskComments.permit(['insert', 'update', 'remove']).apply();
+// Tasks.permit(['insert', 'update', 'remove']).apply();
+// Users.permit(['insert', 'update', 'remove']).apply();
