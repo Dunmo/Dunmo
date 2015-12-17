29fc613ce40d7570380f8333a9be63ea22f81304
Jeremy Meyer <jjman505@gmail.com>
2015-12-17 12:01:52 -0500

    fixed code styles

--- client/lib/helpers.js
+++ client/lib/helpers.js
@@ -13,7 +13,7 @@ Helpers = {
 
   isGmailAddress (email) {
     return email.substring(email.length-10, email.length) === '@gmail.com';
-  }
+  },
 };
 
 Template.registerHelper('isGmailAddress', Helpers.isGmailAddress);
